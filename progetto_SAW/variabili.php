b28af84840e19e65518d7fbd805265ef89a4fbe0
pedemonte953 <pede1994@hotmail.it>
2017-10-18 16:03:44 +0200

    ecco la roba

---
+++ progetto_SAW/variabili.php
@@ -0,0 +1,33 @@
+<?php
+//S:=Sentiero, U:=utente, M:=messaggi, P:=preferiti (utilizzo variabili inizializzate per altri campi), R:=Rating
+//inizializzo tutte la variabili
+//sentiero
+$idS=NULL;
+$nomeS=NULL;
+$regioneS=NULL;
+$provinciaS=NULL;
+$cittaS=NULL;
+$longS=NULL; //coordinate
+$latS=NULL;
+$indirizzoS=NULL;
+$difficoltaS=NULL;
+$descrizioneS=NULL;
+$statoS=NULL; //stato fisico del sentiero, se è in buono stato o se è da "ripulire"
+$fotoS=NULL;
+//user
+$usernameU=NULL;
+$mailU=NULL;
+$passwordU=NULL;
+$nomeU=NULL;
+$cognomeU=NULL;
+$fotoU=NULL;
+$cittaU=NULL;
+$sessoU=NULL;
+//messaggi
+$dataM=NULL;
+$testoM=NULL;
+//rating
+$commentoR=NULL;
+$stelleR=NULL;
+
+?>
